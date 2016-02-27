81160bc4fe1c5c7bde148cfb004476da65fb5800
Dmytro Fominykh <dmytro@lcbrq.com>
2016-02-27 04:33:27 +0100

    Amends for Ceneo

--- app/code/local/LCB/Feeds/Helper/Ceneo.php
+++ app/code/local/LCB/Feeds/Helper/Ceneo.php
@@ -1,14 +1,21 @@
 <?php
 
 /*
- * @category 	LCB
- * @package 	LCB_Feeds
- * @copyright 	Copyright (c) 2015 LeftCurlyBracket (http://www.leftcurlybracket.com/)
+ * @category    LCB
+ * @package     LCB_Feeds
+ * @copyright   Copyright (c) 2015 LeftCurlyBracket (http://www.leftcurlybracket.com/)
  */
 
 class LCB_Feeds_Helper_Ceneo extends Mage_Core_Helper_Abstract
 {
 
+    const DELIVERY_24H = 1;
+    const DELIVERY_72H = 3;
+    const DELIVERY_WEEK = 7;
+    const DELIVERY_DELAY = 14;
+    const DELIVERY_UNKNOWN = 99;
+    const CATEGORY_PATH = 'Biżuteria i zegarki/Biżuteria dla kobiet/Pozostała biżuteria';
+    
     public function getCeneoCategory($product) {
 
         $categories = $product->getCategoryCollection();
@@ -21,7 +28,7 @@ class LCB_Feeds_Helper_Ceneo extends Mage_Core_Helper_Abstract
             $id = $categories->getFirstItem()->getId();
             $category = Mage::getModel('catalog/category')->load($id);
             foreach ($xml as $cat) {
-                return 1109;
+                return self::CATEGORY_PATH;
                 if(empty($cat->Id)){
                     $cat->Id = 1109;
                 }
