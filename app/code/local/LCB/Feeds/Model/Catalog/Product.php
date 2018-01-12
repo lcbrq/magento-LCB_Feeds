addbadb184d31b55951ea5b7e040d8f0d9108251
Tomasz Gregorczyk <tom@lcbrq.com>
2018-01-12 15:42:49 +0100

    Refactor adminhtml configuration and add possibility to include lack of description products in feed

--- app/code/local/LCB/Feeds/Model/Catalog/Product.php
+++ app/code/local/LCB/Feeds/Model/Catalog/Product.php
@@ -6,7 +6,8 @@
  * @copyright 	Copyright (c) 2015 LeftCurlyBracket (http://www.leftcurlybracket.com/)
  */
 
-class LCB_Feeds_Model_Catalog_Product {
+class LCB_Feeds_Model_Catalog_Product
+{
 
     const COLLECTION_LIMIT = 2000;
 
@@ -16,30 +17,34 @@ class LCB_Feeds_Model_Catalog_Product {
         $collection->joinField('is_in_stock', 'cataloginventory/stock_item', 'is_in_stock', 'product_id=entity_id', '{{table}}.stock_id=1', 'left')
                 ->addAttributeToSelect(array('sku', 'name', 'description'))
                 ->addAttributeToFilter('is_in_stock', array('neq' => 0))
-                ->addAttributeToFilter(array(
-                    array(
-                        'attribute' => 'description',
-                        'neq' => '&nbsp;'),
-                    array(
-                        'attribute' => 'short_description',
-                        'neq' => '&nbsp;'),
-                ))
-                ->addAttributeToFilter(array(
-                    array(
-                        'attribute' => 'description',
-                        'null' => false),
-                    array(
-                        'attribute' => 'short_description',
-                        'null' => false),
-                ))
                 ->addAttributeToFilter('visibility', array(
                     'in' => 4
                 ))->addAttributeToFilter(
                 'status', array('eq' => Mage_Catalog_Model_Product_Status::STATUS_ENABLED));
 
+        if (Mage::getStoreConfig('lcb_feeds/general/hide_no_description', Mage::app()->getStore())) {
+            $collection
+                    ->addAttributeToFilter(array(
+                        array(
+                            'attribute' => 'description',
+                            'neq' => '&nbsp;'),
+                        array(
+                            'attribute' => 'short_description',
+                            'neq' => '&nbsp;'),
+                    ))
+                    ->addAttributeToFilter(array(
+                        array(
+                            'attribute' => 'description',
+                            'null' => false),
+                        array(
+                            'attribute' => 'short_description',
+                            'null' => false),
+            ));
+        }
+
         $limit = Mage::app()->getRequest()->getParam('limit') ? Mage::app()->getRequest()->getParam('limit') : self::COLLECTION_LIMIT;
         $collection->getSelect()->limit($limit);
-        
+
         return $collection;
     }
 
